a18dc7d5f3329edc17f9629d4882d2965b568b50
Suganya S <62133935+Subha-21@users.noreply.github.com>
2023-02-16 19:07:51 +0530

    Add files via upload

--- marathon2/OrderingMobile.java
+++ marathon2/OrderingMobile.java
@@ -1,18 +1,23 @@
 package marathon2;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.OutputType;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.Select;
 
 import io.github.sukgu.Shadow;
 
 public class OrderingMobile {
 
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) throws InterruptedException, IOException {
 		// TODO Auto-generated method stub
 
 		ChromeOptions option = new ChromeOptions();
@@ -34,19 +39,55 @@ public class OrderingMobile {
 		shadow.setImplicitWait(30);
 		
 	    shadow.findElementByXPath("//div[text()='All']").click();
-	 
+
+	 //   shadow.findElementByXPath("//div[@class ='sn-polaris-nav-header-filter can-animate']/label[text()='All filter navigator']").sendKeys("Service Catalog", Keys.ENTER);
+
+	    shadow.findElementByXPath("//span[text()='Service Catalog']").click();
 	    
-   
-	    WebElement filter = shadow.findElementByXPath("//div[class()='sn-polaris-nav-header-filter can-animate']/label[text()='All filter navigator']");
-	    filter.click();
-	    filter.sendKeys("Service Catalog");
-	    filter.sendKeys(Keys.ENTER);
+	   WebElement mobile = shadow.findElementByXPath("//iframe[@id ='gsft_main']");
+	    
+	    driver.switchTo().frame(mobile);
+	    
+	    driver.findElement(By.xpath("//h2[contains(text(), 'Mobiles')]")).click();
+	    
+	    driver.findElement(By.xpath("//strong[text()='iPhone 6s']")).click();
+	    
+	    WebElement mobileColor = driver.findElement(By.xpath("//select[@class ='form-control cat_item_option ']"));
+	    
+	    Select mc = new Select(mobileColor);
+	    
+	    mc.selectByVisibleText("Gold");
+	    
+	    WebElement mobileStorage = driver.findElement(By.xpath("(//select[@class='form-control cat_item_option '])[2]"));
+	    
+	    Select ms = new Select(mobileStorage);
+		
+	    ms.selectByVisibleText("128");
+	    
+	    driver.findElement(By.id("oi_order_now_button")).click();
+	    
+	    String orderPlaced = driver.findElement(By.xpath("//div[@class = 'notification notification-success']/span")).getText();
+	    
+	    if (orderPlaced.contains("has been submitted")) {
+	    	
+	    	System.out.println("Order is placed");
+			
+		} else {
+			
+			System.out.println("Order not placed");
 
-
-//        WebElement serviceCatalog = shadow.findElementByXPath("//span[text()='Service Catalog']");
-//       serviceCatalog.click();
-//		
-//		
+		}
+	    
+	    String requestNumber = driver.findElement(By.id("requesturl")).getText();
+	    
+	    System.out.println("Request Number is : " + requestNumber);	    
+	    
+	    File Source = driver.getScreenshotAs(OutputType.FILE);
+	    
+	    File dest = new File("./snaps/Request.png");
+	    
+	    FileUtils.copyFile(Source, dest);
+	
 	}
 
 }
